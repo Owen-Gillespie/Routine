f24c134b6d0d0b12a5cd7d2c056fa83f0cb14570
Owen-Gillespie <owenlgillespie@gmail.com>
2018-01-12 12:40:06 -0500

    Split routine builder into its own component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Cookie from 'js-cookie';
 
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function StringList(props) {
+  const strings = props.strings;
+  const listItems = strings.map((string, index) =>
+    <ListItem key={index}
+              value={string} />
+
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
 class Task extends React.Component {
   constructor(props) {
     super(props);
@@ -19,6 +37,29 @@ class Task extends React.Component {
   }
 }
 
+class RoutineMaker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTaskName : ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="newTask">
+        <StringList strings = {this.props.tasks} />
+        <input ref="newTaskNameInput" type="text" className="newTaskName"
+               value={this.state.newTaskName}
+               onChange={evt => this.setState({newTaskName: evt.target.value})}/>
+        <button onClick={evt => this.props.addTask(this.state.newTaskName)}> Add </button>
+        <button onClick={evt => this.props.save()}> Save </button>
+      </div>
+
+      )
+  }
+}
+
 class View extends React.Component {
   constructor(props) {
     super(props);
@@ -26,23 +67,34 @@ class View extends React.Component {
       tasks: [],
       premade: (typeof Cookie.getJSON('todo') !== 'undefined')
     };
+    if (this.state.premade) { // TODO: Determine why we aren't loading saved state correctly
+      this.setState({tasks: Cookie.getJSON('todo')})
+    }
     this.keyboardListener = this.keyboardListener.bind(this);
+    this.addNewTask = this.addNewTask.bind(this);
+    this.save = this.save.bind(this);
+    this.getCurrentTask = this.getCurrentTask.bind(this)
   }
   
   render() {
+    if (this.state.premade)
+      {
+        return (
+              <div className="view">
+                <Task name={this.getCurrentTask()}/>
+              </div>
+            );
+      }
     return (
-      <div className="view">
-        <div className="currentTask">
-          <Task name={this.getCurrentTask()}/>
-        </div>
-        <div className="newTask">
-          <input ref="newTaskNameInput" type="text" className="newTaskName"
-                 value={this.state.newTaskName}
-                 onChange={evt => this.setState({newTaskName: evt.target.value})}/>
-          <button onClick={evt => this.addNewTask()}> Add </button>
-        </div>
-      </div>
-    );
+      <RoutineMaker tasks={this.state.tasks} addTask={this.addNewTask} save={this.save}/>
+    )
+    
+  }
+
+  save() {
+    Cookie.set('todo', this.state.tasks)
+    console.log(this.state.tasks)
+    this.setState({premade: true})
   }
 
   advance() {
@@ -71,18 +123,19 @@ class View extends React.Component {
     }
   }
 
-  addNewTask() {
+  addNewTask(taskName) {
     let newTasks = this.state.tasks;
     // Currently don't handle empty inputs.
-    newTasks.push(this.state.newTaskName);
+    newTasks.push(taskName);
     // Clear out the name.
-    this.setState({
-      newTaskName: ''
-    });
+    // this.setState({
+    //   newTaskName: ''
     this.setState({tasks: newTasks});
-  }
+  };
 
   getCurrentTask() {
+    console.log(this.state.tasks)
+    console.log(this.state.premade)
     if (this.state.tasks.length !== 0) {
       return this.state.tasks[0];
     } else {
